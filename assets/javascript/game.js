0fe4226444037186eb4219c108b4463fcd537d0c
Alejandro Loja <alejandro.loja@gmail.com>
2018-10-07 02:03:46 -0400

    almost works

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,18 +1,24 @@
-var wins = 0;
-    var losses = 0;
-    var genNumber;
-    var crystalNumber;
-    var totalVal = 0;
-
 $(document).ready(function () {
     //READY TO PLAY
     console.log("ready!");
+    var wins = 0;
+    var losses = 0;
+    var genNumber;
+    var totalVal = 0;
 
-    $(this).attr('value', crystNumGenerator());
+
 
     $('#win').text(wins);
     $('#loss').text(losses);
     $('#total-value').text(totalVal);
+
+
+    for (var i = 0; i < 4; i++) {
+        var crystValNumber = crystNumGenerator();
+        console.log('value of ' + i + 'th image: ' + crystValNumber);
+        $('.img-crystals').append('<img class="crystal" value=' + crystValNumber + ' src="assets/images/crystal-' + i + '.jpg">');
+    };
+
 
     function numberGenerator() {
         genNumber = Math.floor(Math.random() * 102) + 19;
@@ -20,38 +26,52 @@ $(document).ready(function () {
 
     };
 
+
     function crystNumGenerator() {
         return Math.floor(Math.random() * 12) + 1;
-        
+
     }
 
     $('.crystal').on('click', function () {
-        // console.log($(this));
-        totalVal = totalVal + 3;
+        console.log($(this));
+        var crystValue = $(this).attr("value");
+        var crystIntValue = parseInt(crystValue);
+        totalVal = totalVal + crystIntValue;
         $('#total-value').text(totalVal);
-        console.log('totalVal: ' + totalVal + ' and genNumber: ' + genNumber); 
-
+        console.log('totalVal: ' + totalVal + ' and genNumber: ' + genNumber);
+        winnerORloser();
     });
 
-    
+
 
 
     numberGenerator();
 
-    if (totalVal === genNumber) {
-        // userWins();
-        // resetGame();
-        console.log('WON!');
-    }
-    else if (totalVal > genNumber) {
-        // userLosses();
-        // resetGame();
-        console.log('LOST!');
-    }
+    function winnerORloser() {
+        if (totalVal === genNumber) {
+            wins++;
+            $('#win').text(wins);
+            setTimeout(function () {
+                wins++;
+                $('#win').text(wins);
+                $('#did-i-win').text('.')
+            }, 2000);
+            $('#did-i-win').text('You Won!!');
+        }
+        else if (totalVal > genNumber) {
+            losses++;
+            $('#loss').text(losses);
+            // userLosses();
+            // resetGame();
+            console.log('LOST!');
+            setTimeout(function () {
+                $('#did-i-win').text('.')
+            }, 1000);
+            $('#did-i-win').text('You Lost.');
+        }
+    };
 
-    
-
-
+    // resetO
 
     //    // if clicked on generated number then will generate a new number
     // $('#gen-number').on('click', function(){
